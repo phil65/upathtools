416b22c05dd770d68b72693df746346b7488acdf
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-03-16 03:19:30 +0100

    chore: add to top-level

--- src/upathtools/__init__.py
+++ src/upathtools/__init__.py
@@ -1,5 +1,13 @@
 __version__ = "0.4.4"
 
 from upathtools.async_ops import read_path, read_folder, list_files, read_folder_as_text
+from upathtools.httpx_fs import HttpPath, HTTPFileSystem
 
-__all__ = ["list_files", "read_folder", "read_folder_as_text", "read_path"]
+__all__ = [
+    "HTTPFileSystem",
+    "HttpPath",
+    "list_files",
+    "read_folder",
+    "read_folder_as_text",
+    "read_path",
+]
