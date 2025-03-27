80ee28c4cee82fb66deac0bbe1c32bca02bb044d
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-03-27 09:01:03 +0100

    chore: fn to register all filesystems

--- src/upathtools/__init__.py
+++ src/upathtools/__init__.py
@@ -5,6 +5,7 @@ from upathtools.httpx_fs import HttpPath, HTTPFileSystem
 
 
 def register_http_filesystems():
+    """Register HTTP filesystems."""
     from fsspec import register_implementation
     from upath import registry
 
@@ -14,6 +15,49 @@ def register_http_filesystems():
     registry.register_implementation("https", HttpPath, clobber=True)
 
 
+def register_all_filesystems():
+    """Register all filesystem implementations provided by upathtools."""
+    from fsspec import register_implementation
+    from upath import registry
+
+    # Import all filesystem implementations to ensure they're available
+    from upathtools.cli_fs import CliFS, CliPath
+    from upathtools.distribution_fs import DistributionFS, DistributionPath
+    from upathtools.flat_union_fs import FlatUnionFileSystem, FlatUnionPath
+    from upathtools.markdown_fs import MarkdownFS, MarkdownPath
+    from upathtools.module_fs import ModuleFS, ModulePath
+    from upathtools.package_fs import PackageFS, PackagePath
+    from upathtools.python_ast_fs import AstPath, PythonAstFS
+    from upathtools.union_fs import UnionFileSystem, UnionPath
+
+    # Register HTTP filesystems
+    register_http_filesystems()
+    # Register other filesystems
+    register_implementation("cli", CliFS, clobber=True)
+    registry.register_implementation("cli", CliPath, clobber=True)
+
+    register_implementation("distribution", DistributionFS, clobber=True)
+    registry.register_implementation("distribution", DistributionPath, clobber=True)
+
+    register_implementation("flatunion", FlatUnionFileSystem, clobber=True)
+    registry.register_implementation("flatunion", FlatUnionPath, clobber=True)
+
+    register_implementation("md", MarkdownFS, clobber=True)
+    registry.register_implementation("md", MarkdownPath, clobber=True)
+
+    register_implementation("mod", ModuleFS, clobber=True)
+    registry.register_implementation("mod", ModulePath, clobber=True)
+
+    register_implementation("pkg", PackageFS, clobber=True)
+    registry.register_implementation("pkg", PackagePath, clobber=True)
+
+    register_implementation("ast", PythonAstFS, clobber=True)
+    registry.register_implementation("ast", AstPath, clobber=True)
+
+    register_implementation("union", UnionFileSystem, clobber=True)
+    registry.register_implementation("union", UnionPath, clobber=True)
+
+
 __all__ = [
     "HTTPFileSystem",
     "HttpPath",
@@ -21,5 +65,6 @@ __all__ = [
     "read_folder",
     "read_folder_as_text",
     "read_path",
+    "register_all_filesystems",
     "register_http_filesystems",
 ]
