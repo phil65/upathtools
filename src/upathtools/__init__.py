8229b5de5a0393d26e33dd5c05395bd4fca7d7e7
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-04-10 20:50:42 +0200

    Bump version 0.6.2 → 0.6.3

--- src/upathtools/__init__.py
+++ src/upathtools/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 
 from upathtools.async_ops import read_path, read_folder, list_files, read_folder_as_text
 from upathtools.filesystems.httpx_fs import HttpPath, HTTPFileSystem
