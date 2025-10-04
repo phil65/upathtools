b7d751679d98231c76248d95df0fa2aa726b52b9
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-04 03:57:48 +0200

    chore: typing

--- src/upathtools/configs/base.py
+++ src/upathtools/configs/base.py
@@ -131,6 +131,6 @@ class PathConfig(BaseModel):
 if __name__ == "__main__":
     from upathtools.configs.fsspec_fs_configs import ZipFilesystemConfig
 
-    zip_config = ZipFilesystemConfig(fo="C:/Users/phili/Downloads/tags.zip")
+    zip_config = ZipFilesystemConfig(fo=UPath("C:/Users/phili/Downloads/tags.zip"))
     fs = zip_config.create_fs()
     print(fs.ls("tags/"))
