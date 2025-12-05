ca11a96dcde7fc65829a99951776b955b3ad47fc
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-12-05 03:46:14 +0100

    chore: cleanup

--- src/upathtools/configs/__init__.py
+++ src/upathtools/configs/__init__.py
@@ -6,147 +6,21 @@ from typing import Annotated
 
 from pydantic import Field
 
-from upathtools.configs.base import FileSystemConfig, PathConfig
-from upathtools.configs.custom_fs_configs import (
-    AppwriteFilesystemConfig,
-    BaseModelFilesystemConfig,
-    BaseModelInstanceFilesystemConfig,
-    BeamFilesystemConfig,
-    CliFilesystemConfig,
-    DaytonaFilesystemConfig,
-    DistributionFilesystemConfig,
-    E2BFilesystemConfig,
-    FlatUnionFilesystemConfig,
-    GistFilesystemConfig,
-    HttpFilesystemConfig,
-    HttpxFilesystemConfig,
-    MarkdownFilesystemConfig,
-    McpFilesystemConfig,
-    MicrosandboxFilesystemConfig,
-    ModalFilesystemConfig,
-    ModuleFilesystemConfig,
-    NotionFilesystemConfig,
-    OpenApiFilesystemConfig,
-    PackageFilesystemConfig,
-    PythonAstFilesystemConfig,
-    SkillsFilesystemConfig,
-    SRTFilesystemConfig,
-    TypeAdapterFilesystemConfig,
-    UnionFilesystemConfig,
-    VercelFilesystemConfig,
-    WikiFilesystemConfig,
-)
-from upathtools.configs.fsspec_fs_configs import (
-    ArrowFilesystemConfig,
-    DataFilesystemConfig,
-    DaskWorkerFilesystemConfig,
-    FTPFilesystemConfig,
-    GitFilesystemConfig,
-    GithubFilesystemConfig,
-    HadoopFilesystemConfig,
-    JupyterFilesystemConfig,
-    LibArchiveFilesystemConfig,
-    LocalFilesystemConfig,
-    MemoryFilesystemConfig,
-    SFTPFilesystemConfig,
-    SMBFilesystemConfig,
-    TarFilesystemConfig,
-    WebHDFSFilesystemConfig,
-    ZipFilesystemConfig,
-)
+from upathtools.configs.base import FileSystemConfig, PathConfig, URIFileSystemConfig
+from upathtools.configs.custom_fs_configs import CustomFilesystemConfig
+from upathtools.configs.fsspec_fs_configs import FsspecFilesystemConfig
 
+# Combined union of all filesystem config types
 FilesystemConfigType = Annotated[
-    AppwriteFilesystemConfig
-    | BaseModelFilesystemConfig
-    | BaseModelInstanceFilesystemConfig
-    | BeamFilesystemConfig
-    | CliFilesystemConfig
-    | DaytonaFilesystemConfig
-    | DistributionFilesystemConfig
-    | E2BFilesystemConfig
-    | FlatUnionFilesystemConfig
-    | GistFilesystemConfig
-    | HttpFilesystemConfig
-    | HttpxFilesystemConfig
-    | MarkdownFilesystemConfig
-    | McpFilesystemConfig
-    | MicrosandboxFilesystemConfig
-    | ModalFilesystemConfig
-    | ModuleFilesystemConfig
-    | NotionFilesystemConfig
-    | OpenApiFilesystemConfig
-    | PackageFilesystemConfig
-    | PythonAstFilesystemConfig
-    | SkillsFilesystemConfig
-    | SRTFilesystemConfig
-    | TypeAdapterFilesystemConfig
-    | UnionFilesystemConfig
-    | VercelFilesystemConfig
-    | WikiFilesystemConfig
-    | ArrowFilesystemConfig
-    | DataFilesystemConfig
-    | DaskWorkerFilesystemConfig
-    | FTPFilesystemConfig
-    | GitFilesystemConfig
-    | GithubFilesystemConfig
-    | HadoopFilesystemConfig
-    | JupyterFilesystemConfig
-    | LibArchiveFilesystemConfig
-    | LocalFilesystemConfig
-    | MemoryFilesystemConfig
-    | SFTPFilesystemConfig
-    | SMBFilesystemConfig
-    | TarFilesystemConfig
-    | WebHDFSFilesystemConfig
-    | ZipFilesystemConfig,
+    CustomFilesystemConfig | FsspecFilesystemConfig | URIFileSystemConfig,
     Field(discriminator="fs_type"),
 ]
 
 __all__ = [
-    "AppwriteFilesystemConfig",
-    "ArrowFilesystemConfig",
-    "BaseModelFilesystemConfig",
-    "BaseModelInstanceFilesystemConfig",
-    "BeamFilesystemConfig",
-    "CliFilesystemConfig",
-    "DaskWorkerFilesystemConfig",
-    "DataFilesystemConfig",
-    "DaytonaFilesystemConfig",
-    "DistributionFilesystemConfig",
-    "E2BFilesystemConfig",
-    "FTPFilesystemConfig",
+    "CustomFilesystemConfig",
     "FileSystemConfig",
     "FilesystemConfigType",
-    "FlatUnionFilesystemConfig",
-    "GistFilesystemConfig",
-    "GitFilesystemConfig",
-    "GithubFilesystemConfig",
-    "HadoopFilesystemConfig",
-    "HttpFilesystemConfig",
-    "HttpxFilesystemConfig",
-    "JupyterFilesystemConfig",
-    "LibArchiveFilesystemConfig",
-    "LocalFilesystemConfig",
-    "MarkdownFilesystemConfig",
-    "McpFilesystemConfig",
-    "MemoryFilesystemConfig",
-    "MicrosandboxFilesystemConfig",
-    "ModalFilesystemConfig",
-    "ModuleFilesystemConfig",
-    "NotionFilesystemConfig",
-    "OpenApiFilesystemConfig",
-    "PackageFilesystemConfig",
+    "FsspecFilesystemConfig",
     "PathConfig",
-    "PythonAstFilesystemConfig",
-    "SFTPFilesystemConfig",
-    "SMBFilesystemConfig",
-    "SRTFilesystemConfig",
-    "SkillsFilesystemConfig",
-    "TarFilesystemConfig",
-    "TypeAdapterFilesystemConfig",
-    "UnionFilesystemConfig",
-    "VercelFilesystemConfig",
-    "WebHDFSFilesystemConfig",
-    "WikiFilesystemConfig",
-    "ZipFilesystemConfig",
+    "URIFileSystemConfig",
 ]
