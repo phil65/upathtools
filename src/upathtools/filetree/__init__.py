51bf94c7fa0e3088a9b4d8afd6f0b2418aadc517
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-12-10 10:37:48 +0100

    chore: jinjarope filters

--- src/upathtools/filetree/__init__.py
+++ src/upathtools/filetree/__init__.py
@@ -8,5 +8,15 @@ from upathtools.filetree.filetree import (
     DirectoryTree,
     TreeOptions,
 )
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import jinja2
+
+
+def setup_jinjarope_filters(env: jinja2.Environment):
+    """Setup JinjaRope filters for filetree."""
+    env.filters["get_directory_tree"] = get_directory_tree
+
 
 __all__ = ["DirectoryTree", "SortCriteria", "TreeOptions", "get_directory_tree"]
